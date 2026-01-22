c04bb1821c16ceafe13439a44d2f318bf81826fa
Rinngell <rinagel97@gmail.com>
2026-01-22 13:10:03 -0600

    [ADD]Initial Commit

---
+++ src/main/java/com/project/springboot_api_test/SpringbootApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.project.springboot_api_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApiTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/springboot_api_test/SpringbootApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springboot_api_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
